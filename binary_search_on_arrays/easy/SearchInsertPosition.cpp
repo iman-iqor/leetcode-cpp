3a46b8917d9d063bd4010caa42a04f92ce1257d5
Iman Iqor <iqorimane111@gmail.com>
2026-02-24 20:15:23 +0000

    hh

---
+++ binary_search_on_arrays/easy/SearchInsertPosition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() -1;
+        
+    }
+};
+
+int main()
+{
+    vector<int> arr={1,2,3,4,5,6};
+    Solution a;
+    cout<<a.searchInsert(arr,7)<<endl;
+}
