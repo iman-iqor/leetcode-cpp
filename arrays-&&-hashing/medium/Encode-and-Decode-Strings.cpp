456b42073a0cb57e379a262b6ddab25d1b50769e
imiqor <iqorimane111@gmail.com>
2026-09-11 17:42:44 +0100

    end

--- arrays-&&-hashing/medium/Encode-and-Decode-Strings.cpp
+++ arrays-&&-hashing/medium/Encode-and-Decode-Strings.cpp
@@ -1,60 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <string>
-class Solution
-{
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+class Solution {
 public:
-    std::string encode(std::vector<std::string> &strs)
-    {
-        std::string ret;
-        int i  = 0;
+
+    std::string encode(std::vector<std::string>& strs) {
+        std::string str;
+        int i = 0;
+        // std::cout<<"this is the size of strs: "<<strs.size()<<std::endl;
         while(i < strs.size())
         {
-            ret+=std::to_string(strs[i].size());
-            ret+="#";
-            ret+=(strs[i]);
+            str+= std::to_string(strs[i].size())+"#"+strs[i];
             i++;
         }
-        std::cout<<ret<<std::endl;
-        return ret;
+        return str;
     }
 
-
-    std::vector<std::string> decode(std::string s)
-    {
-        std::vector<std::string> ret;
-        
+    std::vector<std::string> decode(std::string s) {
         int i = 0;
-        while(i < s.size())
+        std::vector<std::string> ret;
+        while(i<s.size())
         {
             int j = i;
+            std::string l;
             while(s[j] != '#')
             {
-                
+                l+=s[j];
                 j++;
-                
             }
-            int len = std::stoi(s.substr(i,j-i));
-            j++;
-            ret.push_back(s.substr(j,len));
-           
-            i = j+len;
+            int len = std::stoi(l);
+            int size_of_len = std::to_string(len).size()+1;
+            // std::cout<<"this is the len: "<<len<<" and this is size of len: "<<size_of_len <<std::endl;
+            ret.push_back(s.substr(i+size_of_len,len));
+            i+=len+size_of_len;
+            // std::cout<<"this is i: "<<i<<std::endl;
             
         }
+       
         return ret;
     }
 };
 
 int main()
 {
-    std::vector<std::string> strs={"hello","world"};
     Solution s;
-    std::string str = s.encode(strs);
-    std::vector<std::string> v = s.decode(str);
-   int i = 0;
-   while(i < v.size())
-   {
-        std::cout<<v[i]<<std::endl;
+    std::vector<std::string> v={"we","say",":","yes","!@#$%^&*()"};
+    std::string str=s.encode(v);
+    std::cout<<str<<std::endl;
+    std::vector<std::string> ve = s.decode(str);
+    int i = 0;
+    while(i < ve.size())
+    {
+        std::cout<<ve[i]<<std::endl;
         i++;
-   }
+    }
 }
